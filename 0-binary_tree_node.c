7694db7610e0cdee3a4f9d2a5160342677c3d9f1
polarthebear <thatotae325@gmail.com>
2024-04-11 13:21:42 -0700

    Function that creates a binary tree node.

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -5,7 +5,7 @@
  * Description: Creates a new binary tree node
  * @parent: Pointer to the parent node of the node to create
  * @value: Value to put in the new node
- * Return: Pointer to the new node, or NULL on failure
+ * Return: Pointer to the new node or NULL on failure
  */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
